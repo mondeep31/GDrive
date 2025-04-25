5b503da8e0bf49210e19489ad02f097ad5c3ee9e
Mondeep Chetry <mandeepchetry6969@gmail.com>
2025-04-26 00:20:40 +0530

    fix: removed whitespaces in aws config fallback

--- server/src/utils/s3.ts
+++ server/src/utils/s3.ts
@@ -1,9 +1,9 @@
 import AWS from 'aws-sdk'
 
-const accessKeyId = process.env.AWS_ACCESS_KEY_ID || ' ';
-const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY || ' ';
-const region = process.env.AWS_REGION || ' '
-const bucketName = process.env.S3_BUCKET_NAME || ' '
+const accessKeyId = process.env.AWS_ACCESS_KEY_ID
+const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY 
+const region = process.env.AWS_REGION
+const bucketName = process.env.S3_BUCKET_NAME
 
 if (!accessKeyId || !secretAccessKey || !region || !bucketName){
     throw new Error('Missing required AWS Config');
